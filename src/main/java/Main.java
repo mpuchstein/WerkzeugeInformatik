4e416b64cdbba39c22f352421181b34b56bc3f7b
Matthias Puchstein <matthias@puchstein.bayern>
2025-05-07 01:59:02 +0200

    change Main.java in branch merge-conflict

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,10 +5,9 @@ public class Main {
 
         Car car1 = new Car("red", "BMW", 5);
         Car car2 = new Car("blue", "Audi", 3);
-        Car car3 = new Car("blue", "VW", 3);
+        Car car3 = new Car("pink", "VW", 3);
 
         factory.buildCar(car1);
         factory.buildCar(car2);
     }
 }
-
