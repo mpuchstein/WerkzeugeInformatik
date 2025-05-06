b54f258f421616d10550fe541759b5a423a9a2e7
Matthias Puchstein <matthias@puchstein.bayern>
2025-05-07 01:59:35 +0200

    change Main.java in branch main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,15 +7,14 @@ public class Main {
         Car car2 = new Car("green", "Audi", 3);
         Car car3 = new Car("blue", "VW", 3);
 
-        Car car4 = new Car("yellow", "Renault", 4);
+        Car car4 = new Car("black", "Renault", 4);
 
-        //An dieser Stelle befindet sich nun ein Array
-        //Dieses Array wird mit einem Sortieralgorithmus sortiert
-        //Optimaler Code Yippie
+        // An dieser Stelle befindet sich nun ein Array
+        // Dieses Array wird mit einem Sortieralgorithmus sortiert
+        // Optimaler Code Yippie
 
         factory.buildCar(car1);
         factory.buildCar(car2);
 
     }
 }
-
